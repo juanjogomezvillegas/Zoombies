e9627cde944d5e5e33502997b4dc28b2d1b60da0
juanjogomezvillegas <juanjogomvil@gmail.com>
2021-05-10 20:34:53 +0200

    main

--- Zoombies.java
+++ Zoombies.java
@@ -1,10 +1,4 @@
 package Zoombies;
-
-import acm.program.*;
-import acm.graphics.*;
-import java.awt.*;
-import java.util.concurrent.ThreadLocalRandom;
-
 /**
  * @author Juan José Gómez Villegas
  * @author Jorge Luís Martínez Bermudez
@@ -13,16 +7,47 @@ import java.util.concurrent.ThreadLocalRandom;
  * @author Kirill Lupenkov
  * **/
 
+/**Importa les següents classes:
+ * @see acm.program
+ * @see acm.graphics
+ * @see java.awt
+ * @see java.util.concurrent.ThreadLocalRandom
+ * **/
+import acm.program.*;
+import acm.graphics.*;
+import java.awt.*;
+import java.util.concurrent.ThreadLocalRandom;
+
+/**
+ * Crea la Classe "Zoombies", heretada de la classe "GraphicsProgram"
+ * @see GraphicsProgram
+ * @version 1
+ * **/
 public class Zoombies extends GraphicsProgram {
+    /**Crea les variables de tipus protected**/
     protected String ruta = "src/Zoombies/";
 
+    /**
+     * Crea el metode main (Principal)
+     * @param args
+     */
     public static void main(String[] args) {new Zoombies().start(args);}
 
+    /**
+     * Crea el metode setter "init"
+     * **/
     public void init() {
+        /**
+         * setSize: Estableix el tamany de la finestra en 975x750
+         * setBackground: Estableix el color de fons de la finestra en el color especificat
+         * **/
         setSize(900, 900);
         setBackground(Color.LIGHT_GRAY);
     }
 
+    /**
+     * Crea el metode setter "run"
+     * **/
     public void run() {
         GImage zoombie = getRetornaZoombie();
         add(zoombie);
@@ -36,10 +61,18 @@ public class Zoombies extends GraphicsProgram {
     }
 }
 
+/**
+ * Crea la Classe "Aleatori"
+ * @version 1
+ * **/
 class Aleatori {
-    /*Crea el metode getter "getNumeroAleatori"*/
+    /**
+     * Crea el metode getter "getNumeroAleatori", que retornara un numero aleatori entre el "min" i el "max" (parametres)
+     * @param min
+     * @param max
+     * @return ThreadLocalRandom.current().nextInt(min, max)
+     * **/
     public static int getNumeroAleatori(int min, int max) {
-        /*Retorna un numero aleatori entre el "min" i el "max" (parametres) i el retorna*/
         return ThreadLocalRandom.current().nextInt(min, max);
     }
 }

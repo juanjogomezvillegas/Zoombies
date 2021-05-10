df4fee6a0c51bde43dc8d3f697b295fcf33c6d48
juanjogomezvillegas <juanjogomvil@gmail.com>
2021-05-10 19:10:07 +0200

    main

--- Zoombies.java
+++ Zoombies.java
@@ -20,17 +20,25 @@ public class Zoombies extends GraphicsProgram {
 
     public void init() {
         setSize(900, 900);
-        setBackground(Color.PINK);
+        setBackground(Color.LIGHT_GRAY);
     }
 
     public void run() {
-        GImage zoombie = new GImage(ruta+"zoombie.png");
-        zoombie.setSize(50, 50);
+        GImage zoombie = getRetornaZoombie();
         add(zoombie);
     }
 
+    public GImage getRetornaZoombie() {
+        GImage zoombie = new GImage(ruta+"zoombie.png");
+        zoombie.setSize(50, 50);
+        zoombie.setLocation(Aleatori.getNumeroAleatori(1, 800), Aleatori.getNumeroAleatori(1, 800));
+        return zoombie;
+    }
+}
+
+class Aleatori {
     /*Crea el metode getter "getNumeroAleatori"*/
-    public int getNumeroAleatori(int min, int max) {
+    public static int getNumeroAleatori(int min, int max) {
         /*Retorna un numero aleatori entre el "min" i el "max" (parametres) i el retorna*/
         return ThreadLocalRandom.current().nextInt(min, max);
     }

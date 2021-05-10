693f321623a24ef0aca6f183ff5137b56ff31182
juanjogomezvillegas <juanjogomvil@gmail.com>
2021-05-10 18:43:10 +0200

    main

---
+++ Zoombies.java
@@ -0,0 +1,19 @@
+package Zoombies;
+
+import acm.program.*;
+import acm.graphics.*;
+
+import java.awt.*;
+
+public class Zoombies extends GraphicsProgram {
+    public static void main(String[] args) {new Zoombies().start(args);}
+
+    public void init() {
+        setSize(900, 900);
+        setBackground(Color.LIGHT_GRAY);
+    }
+
+    public void run() {
+        //
+    }
+}

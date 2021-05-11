6812647d3e1b3d073eaa6d039fb865c34cd93f19
juanjogomezvillegas <juanjogomvil@gmail.com>
2021-05-11 21:48:42 +0200

    main

--- Zoombies.java
+++ Zoombies.java
@@ -7,7 +7,8 @@ package Zoombies;
  * @author Kirill Lupenkov
  * **/
 
-/**Importa les següents classes:
+/**
+ * Importa les següents classes:
  * @see acm.program
  * @see acm.graphics
  * @see java.awt
@@ -28,10 +29,13 @@ import java.util.concurrent.ThreadLocalRandom;
 public class Zoombies extends GraphicsProgram {
     /**Crea les variables de tipus protected**/
     protected String ruta = "src/Zoombies/";
+    /**Crea les variables de tipus private i constants**/
+    private final int max_move = 700;
+    private final int min_move = 1;
 
     /**
      * Crea el metode main (Principal)
-     * @param args
+     * @param args Array de String amb el nom de "args"
      */
     public static void main(String[] args) {new Zoombies().start(args);}
 
@@ -39,10 +43,10 @@ public class Zoombies extends GraphicsProgram {
      * Crea el metode setter "init"
      * **/
     public void init() {
-        /**
-         * setSize: Estableix el tamany de la finestra en 975x750
-         * setBackground: Estableix el color de fons de la finestra en el color especificat
-         * **/
+        /*
+        * setSize: Estableix el tamany de la finestra en 975x750
+        * setBackground: Estableix el color de fons de la finestra en el color especificat
+        * */
         setSize(900, 900);
         setBackground(Color.LIGHT_GRAY);
     }
@@ -51,16 +55,23 @@ public class Zoombies extends GraphicsProgram {
      * Crea el metode setter "run"
      * **/
     public void run() {
+        Emoji emoji = new Emoji();
+
         GImage zoombie = getRetornaZoombie();
         add(zoombie);
 
-        ArrayList<GImage> arrayEmojis = getArrayEmojis();
+        ArrayList<GImage> arrayEmojis = emoji.getRetornaArrayEmoji();
 
-        for (int i = 0; i <= 100; i++) {
-            int emoji = Aleatori.getNumeroAleatori(0, arrayEmojis.size()-1);
-            arrayEmojis.get(emoji).setLocation(Aleatori.getNumeroAleatori(1, 700), Aleatori.getNumeroAleatori(1, 700));
-            add(arrayEmojis.get(emoji));
-            arrayEmojis.get(emoji).pause(200);
+        int posicio;
+        for (int i = 1; i <= 300; i++) {
+            zoombie.setLocation(Aleatori.getNumeroAleatori(min_move, max_move), Aleatori.getNumeroAleatori(min_move, max_move));
+
+            posicio = emoji.getRetornaPosicioAleatoria();
+            arrayEmojis.get(posicio).setLocation(Aleatori.getNumeroAleatori(min_move, max_move), Aleatori.getNumeroAleatori(min_move, max_move));
+            add(arrayEmojis.get(posicio));
+
+            zoombie.pause(50);
+            arrayEmojis.get(posicio).pause(50);
         }
     }
 
@@ -71,16 +82,40 @@ public class Zoombies extends GraphicsProgram {
     public GImage getRetornaZoombie() {
         GImage zoombie = new GImage(ruta+"zoombie.png");
         zoombie.setSize(50, 50);
-        zoombie.setLocation(Aleatori.getNumeroAleatori(1, 700), Aleatori.getNumeroAleatori(1, 700));
+        zoombie.setLocation(Aleatori.getNumeroAleatori(min_move, max_move), Aleatori.getNumeroAleatori(min_move, max_move));
         return zoombie;
     }
+}
+
+/**
+ * Crea la Classe "Aleatori"
+ * @version 1
+ * **/
+class Aleatori {
+    /**
+     * Crea el metode getter "getNumeroAleatori", que retornara un numero aleatori entre el "min" i el "max" (parametres)
+     * @param min valor minim
+     * @param max valor maxim
+     * @return un numero aleatori entre els parametres "min" i "max"
+     * **/
+    public static int getNumeroAleatori(int min, int max) {return ThreadLocalRandom.current().nextInt(min, max);}
+}
+
+/**
+ * Crea la Classe "Emoji", que esta heretant de la classe "Zoombies"
+ * @see Zoombies
+ * @version 1
+ * **/
+class Emoji extends Zoombies {
+    /**Crea les variables de tipus private**/
+    private ArrayList<GImage> arrayEmojis;
 
     /**
-     * Crea el metode getter "getArrayEmojis"
-     * @return arrayEmojis (de tipus ArrayList<GImage>)
+     * Crea el metode getter "getRetornaEmoji"
+     * @return l'ArrayList "arrayEmojis"
      * **/
-    public ArrayList<GImage> getArrayEmojis() {
-        ArrayList<GImage> arrayEmojis = new ArrayList<>();
+    public ArrayList<GImage> getRetornaArrayEmoji() {
+        arrayEmojis = new ArrayList<>();
 
         arrayEmojis.add(new GImage(ruta+"emoji1.png"));
         arrayEmojis.add(new GImage(ruta+"emoji2.png"));
@@ -94,18 +129,10 @@ public class Zoombies extends GraphicsProgram {
 
         return arrayEmojis;
     }
-}
 
-/**
- * Crea la Classe "Aleatori"
- * @version 1
- * **/
-class Aleatori {
     /**
-     * Crea el metode getter "getNumeroAleatori", que retornara un numero aleatori entre el "min" i el "max" (parametres)
-     * @param min
-     * @param max
-     * @return ThreadLocalRandom.current().nextInt(min, max)
+     * Crea el metode getter "getRetornaEmoji"
+     * @return numero aleatori entre 0 i la longitud de l'ArrayList "arrayEmojis"
      * **/
-    public static int getNumeroAleatori(int min, int max) {return ThreadLocalRandom.current().nextInt(min, max);}
+    public int getRetornaPosicioAleatoria() {return Aleatori.getNumeroAleatori(0, arrayEmojis.size()-1);}
 }
